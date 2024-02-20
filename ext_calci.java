644dd1c407f2736f5591bf1078ecbd492b32c668
Archi <01fe22bca222@kletech.ac.in>
2024-02-20 11:33:16 +0530

    extended code for calci

---
+++ ext_calci.java
@@ -0,0 +1,11 @@
+class ext_calci {
+    float square(float operand_1){
+        return (operand_1*operand_1);
+    }
+
+    float cube(float operand_2){
+        return (operand_2*operand_2*operand_2);
+    }
+}
+    
+
